44c73380d64d38100c7cc965ee2c10005e3d1c70
Chris Baquol <chris@wattsinnovations.com>
2023-03-09 22:30:43 -0500

    added lots of notes and doc references. added input from user for test parameters. updated log file path structure.

--- datalogging/k_type_thermocouple_temp_logging.py
+++ datalogging/k_type_thermocouple_temp_logging.py
@@ -1,21 +1,34 @@
 ################################################
+#
 # simple python code to read a K-type thermocouple and log
 # to a CSV. Can be used for multiple applications.
-##
-# chris baquol 3.8.2023
+#
+# tested only on windows
+#
+# must first install LabJackPython: https://old3.labjack.com/support/software/examples/ud/labjackpython
+#
+# thermocouple used: https://media.digikey.com/pdf/Data%20Sheets/Digilent%20PDFs/240-080_Web.pdf
+#
+# details of k-type thermocouple measurments used in this script originated from LabJackPython example: https://github.com/labjack/LabJackPython/blob/master/Examples/ktypeExample.py
+#
+# watts innovations - chris baquol 3.8.2023
+#
 ###############################################
 
-# must pip install LabJackPython first
+# general imports
 
 import math
-import os
 import time
 import csv
+import pathlib
+
+# labjaack specific import for U6 device
 
 import u6
 
 
-# Coefficients
+# Coefficients used later to convert mV to temp
+# based on cold junction compensation
 
 # -200 C to 0 C
 # -5.891 mV to 0 mV
@@ -28,6 +41,7 @@ import u6
 # -8.6632643E-2
 # -1.0450598E-2
 # -5.1920577E-4
+
 voltsToTemp1 = (0.0E0,
                 2.5173462E1,
                 -1.1662878E0,
@@ -50,6 +64,7 @@ voltsToTemp1 = (0.0E0,
 # -4.41303E-5
 # 1.057734E-6
 # -1.052755E-8
+
 voltsToTemp2 = (0.0E0,
                 2.508355E1,
                 7.860106E-2,
@@ -70,6 +85,7 @@ voltsToTemp2 = (0.0E0,
 # -9.650715E-4
 # 8.802193E-6
 # -3.11081E-8
+
 voltsToTemp3 = (-1.318058E2,
                 4.830222E1,
                 -1.646031E0,
@@ -79,6 +95,7 @@ voltsToTemp3 = (-1.318058E2,
                 -3.11081E-8)
 
 
+# verifies mV reading is within expected value range
 def voltsToTempConstants(mVolts):
     if mVolts < -5.891 or mVolts > 54.886:
         raise Exception("Invalid range")
@@ -102,6 +119,7 @@ def voltsToTempConstants(mVolts):
 # -0.104516094E-16
 # -0.198892669E-19
 # -0.163226975E-22
+
 tempToVolts1 = (0.0E0,
                 0.39450128E-1,
                 0.236223736E-4,
@@ -130,6 +148,7 @@ tempToVolts1 = (0.0E0,
 # 0.1185976E0
 # -0.1183432E-3
 # 0.1269686E3
+
 class ExtendedList(list):
     def __init__(self):
         list.__init__(self)
@@ -149,6 +168,8 @@ tempToVolts2.append(0.971511472E-22)
 tempToVolts2.append(-0.121047213E-25)
 tempToVolts2.extended = (0.1185976E0, -0.1183432E-3, 0.1269686E3)
 
+# verified temp is within expected value range
+
 
 def tempToVoltsConstants(tempC):
     if tempC < -270 or tempC > 1372:
@@ -158,6 +179,8 @@ def tempToVoltsConstants(tempC):
     else:
         return tempToVolts2
 
+# used to convert between mv and temp
+
 
 def evaluatePolynomial(coeffs, x):
     tot = 0
@@ -166,6 +189,8 @@ def evaluatePolynomial(coeffs, x):
         tot += y * a
         y *= x
     return tot
+
+# uses coefficients to calculate temp
 
 
 def tempCToMVolts(tempC):
@@ -177,10 +202,14 @@ def tempCToMVolts(tempC):
     else:
         return evaluatePolynomial(coeffs, tempC)
 
+# calculate temp from mv
+
 
 def mVoltsToTempC(mVolts):
     coeffs = voltsToTempConstants(mVolts)
     return evaluatePolynomial(coeffs, mVolts)
+
+# calculate seconds to hz
 
 
 def hz_to_sec(freq_hz):
@@ -188,60 +217,87 @@ def hz_to_sec(freq_hz):
     return period_sec
 
 
+# main
 if __name__ == '__main__':
 
-    loggingRateHz = 1
-
+    # configures LabJackU6
     d = u6.U6()
     d.getCalibrationData()
 
-    timestamp = time.strftime(
-        "%Y-%m-%d %H.%M.%S", time.localtime())
+    # gets current timestamp used in log file name
+    timestamp = time.strftime("%Y-%m-%d %H.%M.%S", time.localtime())
 
-    logPath = os.getcwd() + "\\temperature_log" + timestamp+".csv"
+    # prints intro information to console
+    print("\n\n\n########################################################")
+    print("\nTemperature logging script using LabJack U6")
+    print("and K-Type Thermocouple connected to AIN0")
+    print("\n########################################################\n")
 
-    print("########################################################")
-    print("Temperature logging script using LabJack U6 and K-Type Thermocouple connected to AIN0")
+    # sets up log file path
+    fileName = input("Enter log file name (automatically saved as .csv): ")
+    logPath = str(pathlib.Path(__file__).parent.resolve()) + \
+        "\\" + fileName + "-" + timestamp + ".csv"
+
+    # sets up logging rate
+    while True:
+        try:
+            loggingRateHz = int(input("Enter desired logging rate in Hz: "))
+            break
+        except ValueError:
+            print("Invalid value.\n")
+
+    # displays user chosen test parameters
     print("\nSaving data at " + str(loggingRateHz) + "Hz")
     print("\nLog file: " + logPath)
-    print("\nPress Ctrl+C at any point to stop logging and close logfile.\n")
-    print("\n########################################################")
 
+    # waits for user to begin test
+    input("\nPress 'Enter' to begin logging. Press 'Ctrl+C' at any point to stop.")
+
+    # opens log file in write mode
     with open(logPath, mode='w', newline='') as logFile:
+
+        # sets up log file
         logWriter = csv.writer(logFile, delimiter=',',
                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        # writes headers to log file
         logWriter.writerow(['Time', 'Temperature (C)'])
 
+        # prints formatted header to console
         print("\n\nTime\t\t\t\tTemperature(C)")
 
+        # sets up keyboard interrupt to stop data collection loop
         try:
+            # begin data collection loop
             while True:
+                # gets current timestamp
                 timestamp = time.strftime(
                     "%Y-%m-%d %H:%M:%S", time.localtime())
 
-                # The cold junction temperature
-                # Important: Must be in Celsius
+                # reads cold junction temperature
                 CJTEMPinC = d.getTemperature() + 2.5 - 273.15
 
-                # The thermocouple's analog voltage
-                # Important: Must be in millivolts
+                # reads analog voltage from thermocouple
                 TCmVolts = d.getAIN(
                     0, resolutionIndex=8, gainIndex=3) * 1000
 
-                # print("Cold Junction Temp: %s" % CJTEMPinC)
-                # print("Voltage (in millivolts): %s" % TCmVolts)
-
+                # calculates mv required to calculate temp
                 totalMVolts = TCmVolts + tempCToMVolts(CJTEMPinC)
 
+                # calculates temp in celsius
                 temp = mVoltsToTempC(totalMVolts)
 
-                print(timestamp + "\t\t" + "%s" % temp)
+                # prints timestamp and temp data to console
+                print(timestamp + "\t\t" + "%.2f" % temp + " Deg (C)")
 
+                # writes data to new row in logfile
                 logWriter.writerow([timestamp, temp])
-                time.sleep(hz_to_sec(loggingRateHz))
+
+                # waits the specified time before reading new data values
+                time.sleep(hz_to_sec(int(loggingRateHz)))
                 pass
 
+        # user enters ctrl + c to stop logging
         except KeyboardInterrupt:
-            print("\nStopping...")
-            print("Logfile: " + logPath + " saved.")
-
+            print("\nLogging stopped...")
+            print("\n\n" + logPath + " saved.\n\n")
