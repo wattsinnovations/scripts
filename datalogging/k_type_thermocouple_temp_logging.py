88dce69a93a6c04a59a943ea17065261dc221436
Chris Baquol <chris@wattsinnovations.com>
2023-03-15 15:05:46 -0400

    plot works. logging works. probably needs further review but works for now

--- datalogging/k_type_thermocouple_temp_logging.py
+++ datalogging/k_type_thermocouple_temp_logging.py
@@ -18,8 +18,11 @@
 #
 # Watts Innovations - Chris Baquol + Shacub 3.8.2023
 #
+# This is a work in progress
+# 
 ###############################################
 
+# imports
 import math
 import time
 import csv
@@ -40,7 +43,24 @@ labjack = u6.U6()
 labjack.getCalibrationData()
 typeK = thermocouples['K']
 
-# gets the temp
+# sets up arg parser
+parser = argparse.ArgumentParser(description='Logs temperature data using a LabJack U6 and K-type Thermocouple connected to AIN0.')
+parser.add_argument('-f', '--filename', type=str, help='file name to save log')
+parser.add_argument('-r', '--rate', type=float, default=20, help='rate to save data to log in Hz')
+args = parser.parse_args()
+
+# setting default value to 20 actually results in logging rate of 10hz ?
+loggingRateHz = args.rate
+
+# Set up the plot
+fig, ax = plt.subplots()
+x_data, y_data = [], []
+ln, = plt.plot([], [], 'ro')
+
+
+############
+## gets the temp
+############
 def getTemp():
     # Reads cold junction temperature in Kelvin
     coldJunctTempC = labjack.getTemperature() + 2.5 - 273.15
@@ -52,25 +72,27 @@ def getTemp():
     tempC = typeK.inverse_CmV(thermoCoupleMillvolt, Tref=coldJunctTempC)
     return tempC
 
-# reads your wrist watch
+############
+## gets the time
+############
 def getTime():
-    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    timestamp = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return timestamp
 
-################################################################################################################
-##
-## matplotlib stuff
-##
-# Set up the plot
-fig, ax = plt.subplots()
-x_data, y_data = [], []
-ln, = plt.plot([], [], 'ro')
 
+############
+## init used for setting up plot
+############
 def init():
     ax.set_xlim(dt.datetime.now(), dt.datetime.now() + dt.timedelta(seconds=10))
     ax.set_ylim(0, 100)
     return ln,
 
+############
+## update runs in main
+## displays values on plot
+## writes to log file
+############
 def update(frame):
     x_data.append(getTime())
     y_data.append(getTemp())
@@ -78,31 +100,33 @@ def update(frame):
 
     window_size = dt.timedelta(seconds=10)
  
-  #  print("X:" + str(x_data[-1]) + "\t\tY: "+ str(y_data[-1])) 
     ax.set_xlim(dt.datetime.now() - window_size, dt.datetime.now() + dt.timedelta(seconds=1))
 
     # for some reason it needs this otherwise it doesnt show datapoints on graph
-    plt.pause(.01)
+    plt.pause(.0000000001)
+   
+    time_now = getTime()
+    tempC = getTemp()
+
+    print(time_now + "\t\t" + "%.2f" % tempC + " Deg (C)")
+
+    logWriter.writerow([time_now, tempC])
+
+    time.sleep((1/loggingRateHz))
    
     return ln,
-##
-##
-###############################################################################################################
 
-
+############
+## main
+############
 if __name__ == '__main__':
 
     print("\n\n\n########################################################")
     print("\nTemperature logging script using LabJack U6")
     print("and K-Type Thermocouple connected to AIN0")
+     # changed below to reflect actual logging rate. default value of 20 is actually 10hz
+    print("\nSaving data at " + str(loggingRateHz/2) + "Hz")
     print("\n########################################################\n")
-
-    parser = argparse.ArgumentParser(description='Logs temperature data using a LabJack U6 and K-type Thermocouple connected to AIN0.')
-    parser.add_argument('-f', '--filename', type=str, help='file name to save log')
-    parser.add_argument('-r', '--rate', type=int, default=10, help='rate to save data to log in Hz')
-    args = parser.parse_args()
-    
-    
 
     if args.filename:
         fileName = args.filename
@@ -112,26 +136,8 @@ if __name__ == '__main__':
     timestamp = time.strftime("%Y-%m-%d %H.%M.%S", time.localtime())
     logPath = str(pathlib.Path(__file__).parent.resolve()) + "\\" + fileName + "-" + timestamp + ".csv"
 
-    loggingRateHz = args.rate
-   
-    print("\nSaving data at " + str(loggingRateHz) + "Hz")
-    print("\nLog file: " + logPath)
     
-    
-##############################################################################################    
-## this section shows the plot
-## it seems like if the plot opens, the script never makes it to the point where it saves data to the log
-## commenting out and not showing plot will save data to log normally
-##
-## maybe need to get even more complex to do both at once
-## this is getting out of hand !!!!! LOLOLOLOLOL
-##
-    ani = FuncAnimation(fig, update, init_func=init, blit=True, interval=(1/loggingRateHz), cache_frame_data=False)
-##           
-    plt.show()
-##
-##############################################################################################    
-
+    # opens the log file, opens the plot, writes data
     with open(logPath, mode='w', newline='') as logFile:
 
         logWriter = csv.writer(logFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -142,32 +148,13 @@ if __name__ == '__main__':
         print("\n\nTime\t\t\t\tTemperature(C)")
 
         
-        try:
-            
-            
-            while True:
-  
-                ## moved stuff below into functions, leaving here for now also
-                ##
-                ###timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+       
+        ani = FuncAnimation(fig, update, init_func=init, blit=True, interval=((1/loggingRateHz)), cache_frame_data=False)
+    
+        plt.show()
 
-                # Reads cold junction temperature in Kelvin
-                ###coldJunctTempC = labjack.getTemperature() + 2.5 - 273.15
 
-                # Reads analog voltage from thermocouple
-                ###thermoCoupleMillvolt = labjack.getAIN(0, resolutionIndex=8, gainIndex=3) * 1000
-               
-                # Use measured millivolts and cold junction temperature to caclulate thermocouple temperature
-                ###tempC = typeK.inverse_CmV(thermoCoupleMillvolt, Tref=coldJunctTempC)
-                                
-                time_now = getTime()
-                tempC = getTemp()
+        
+print("\n\nLogging stopped.....\n\n")
 
-                print(time_now + "\t\t" + "%.2f" % tempC + " Deg (C)")
-
-                logWriter.writerow([timestamp, tempC])
-
-                time.sleep(1/loggingRateHz)
-
-        except KeyboardInterrupt:
-            print("\n\nLogging stopped.....\n\n")
+print("\nLog file: " + logPath+"\n\n")
