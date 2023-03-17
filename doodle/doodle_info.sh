824e2655de507893031ae8dbcfbeb0b9ccb6ff8f
Chris Baquol <chris@wattsinnovations.com>
2023-03-17 14:22:15 -0400

    added cpu load to doodle_info.sh

--- doodle/doodle_info.sh
+++ doodle/doodle_info.sh
@@ -9,6 +9,7 @@ FIRMWARE_FILE_PATH=""
 BANDWIDTH="15"
 #STATION=""
 #$info=""
+
 # Circumvents host key check, silences warning, connection timeout, silence warnings
 SSH_OPTS="-o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedKeyTypes=+ssh-rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ConnectTimeout=2 -o LogLevel=ERROR"
 
@@ -30,8 +31,15 @@ if [ "$TARGET_IP" == "q" ]; then
     exit
 fi 
 
+# gets mac of connected station
 station=$(run_on_target "iwinfo wlan0 assoclist | awk '{print $1}' | cut -c 1-17 | head -n 1")
+
+# gets signal strength in rssi
 rssi=$(run_on_target "iw wlan0 station dump | grep signal | cut -c 12-14 | head -n 1")
+
+# gets cpu load in percentage
+cpu_load =$(tun_on_target "vmstat | awk '{print $13}' | head -n 3 | tail -n 1")
 
 echo "Station: $station" 
 echo "RSSI: $rssi"
+echo "CPU Load: $cpu_load"
